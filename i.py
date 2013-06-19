f25e7a24d75498f704ddd102ef556356a5f560c1
felloak <felloak@gmail.com>
2013-06-19 12:49:41 +0000

    log callback system, unstable

--- i.py
+++ i.py
@@ -20,10 +20,11 @@ class I(dict):
     5
     >>> i.bar
     []
+    >>> i.listener["jump"].add("x")
 
     """
 
-    __slots__ = ["i", "logs"]
+    __slots__ = ["i", "logs", "listener"]
 
     def __init__(self, n, source):
         assert isinstance(source, dict)
@@ -33,6 +34,7 @@ class I(dict):
         i = int(n)
         object.__setattr__(self, "i", i)
         object.__setattr__(self, "logs", collections.deque(maxlen=100))
+        object.__setattr__(self, "listener", collections.defaultdict(set))
 
     def __setattr__(self, k, v):
         self[k] = v
@@ -63,6 +65,39 @@ class I(dict):
     def _wrap_a(raw):
         return str(raw)
 
+
+
+
+_cbs = {}
+
+def handle_log_callback(cb):
+    _cbs[cb.__name__] = cb
+    return cb
+
+def bind(i, log_name, callback_name):
+    if callable(callback_name):
+        callback_name = callback_name.__name__
+    i.listener[log_name].add(callback_name)
+
+def unbind(i, log_name, callback_name):
+    if callable(callback_name):
+        callback_name = callback_name.__name__
+    i.listener[log_name].remove(callback_name)
+
+def log(i, k, n, infos):
+    yield "log", i.i, k, n, infos
+    cb_set = i.listener[k]
+    if cb_set:
+        for callback_name in cb_set:
+            # 3.3+: yield from
+            for x in _cbs[callback_name](i, k, n, infos, callback_name):
+                yield x
+
+@handle_log_callback
+def callback_example(i, k, n, infos, callback_name):
+    print(i, k, n, infos, callback_name)
+
+print(_cbs)
 
 if __name__ == "__main__":
     print("doctest:")
