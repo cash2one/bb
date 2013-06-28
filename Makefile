a596911dfac9b1e02ac5e596448f40f9dfee9b0c
felloak <felloak@gmail.com>
2013-06-28 23:40:10 +0000

    turn into unittest
    and use make
    test/test.py is copy from Python Doc

---
+++ Makefile
@@ -0,0 +1,15 @@
+#PATH := build/python/bin:$(PATH)
+#VERSION = $(shell python setup.py --version)
+#ALLFILES = $(shell echo bottle.py test/*.py test/views/*.tpl)
+
+.PHONY: test clean
+
+test:
+	python -m unittest
+
+clean:
+	rm -rf build/ dist/ MANIFEST 2>/dev/null || true
+	find * -name '*.pyc' -delete
+	find * -name '*.pyo' -delete
+	find * -name 'out' -delete
+
---
+++ test/test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import unittest
+import i
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = list(range(10))
+
+    def test_shuffle(self):
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, list(range(10)))
+
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+
