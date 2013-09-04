1ec491941ffe51c037998b764f76c50acc6bf17c
felloak <felloak@gmail.com>
2013-09-04 16:46:44 +0800

    i: improve event system

--- bb/i.py
+++ bb/i.py
@@ -50,37 +50,25 @@ class I(dict):
     >>> i.logs.append("over")
     >>> #i.listeners["foo"].add("bar")
     >>> i.bind("go", "callback_example", 1)
-    >>> i.listeners["go"] == set([("callback_example", (1,))])
+    >>> i.listeners["go"] == set([("callback_example", 1)])
     True
     >>> i.unbind("go", "callback_example", 1)
     >>> i.listeners["go"] == set()
     True
-    >>> i.bind("go", callback_example, 1, 2, 3)
-    >>> i.bind("go", callback_example2)
-    >>> i.listeners["go"] == set([("callback_example", (1, 2, 3)), ("callback_example2", ())])
+    >>> i.bind("go", callback_example, (1, 2, 3))
+    >>> i.bind("go", callback_example2, None)
+    >>> i.listeners["go"] == set([("callback_example", (1, 2, 3)), ("callback_example2", None)])
     True
     >>> i.log("go")
     >>> len(i.cache)
     5
-    >>> i.listeners["go"] == set([("callback_example2", ())])
+    >>> i.listeners["go"] == set([("callback_example2", None)])
     True
 
-    >>> i.bind("gogogo", callback_example)
-    >>> i.bind("gogogogo", callback_example)
+    >>> i.bind("gogogo", callback_example, None)
+    >>> i.bind("gogogogo", callback_example, None)
     >>> len(i.listeners)
     3
-    >>> check(i, "tower", "i.a > 1", {"i": i}, f, "gogo")
-    >>> len(i.listeners["gogo"])  # daemon launched
-    1
-    >>> i["a"] = 1
-    >>> i.log("gogo")  # "i["a"] > 1" is False, daemon is watching
-    >>> len(i.listeners["gogo"])
-    1
-    >>> i["a"] = 2
-    >>> i.log("gogo")
-    >>> len(i.listeners["gogo"])  # daemon quited
-    0
-
     """
 
     __slots__ = ["_i", "_cache", "_logs", "_listeners"]
@@ -153,23 +141,19 @@ class I(dict):
     def listeners(self):
         return self._listeners
 
-    def bind(self, log, cb, *args):
+    def bind(self, log, cb, extra):
         if callable(cb):
             cb = cb.__name__
         assert isinstance(log, str), log
         assert cb in _cbs, cb
-        cb_args = cb, args
-        self.listeners[log].add(cb_args)
+        self.listeners[log].add((cb, extra))
 
-    def unbind(self, log, cb, *args):
+    def unbind(self, log, cb, extra):
         if callable(cb):
             cb = cb.__name__
         assert isinstance(log, str), log
         assert cb in _cbs, cb
-        all_cb_args = self.listeners[log]
-        cb_args = cb, args
-        if cb_args in all_cb_args:
-            all_cb_args.remove(cb_args)
+        self.listeners[log].discard((cb, extra))
 
     def send(self, k, v):
         assert isinstance(k, str), k
@@ -188,10 +172,8 @@ class I(dict):
         assert isinstance(n, int), n
         self.cache.append(["log", self.i, k, infos, n])
         self.logs.append([k, infos, n])
-        all_cb_args = self.listeners[k]
-        if all_cb_args:
-            for cb_args in list(all_cb_args):   # need a copy for iter
-                _cbs[cb_args[0]](self, k, infos, n, *cb_args[1])
+        for cb in list(self.listeners[k]):          # need a copy for iter
+            _cbs[cb[0]](self, k, infos, n, cb[1])   # cb may change listeners[k]
 
     def render(self, rc):
         """
@@ -365,36 +347,15 @@ class I(dict):
 
 # examples here:
 @register_log_callback
-def callback_example(i, k, infos, n, *args):
-    i.unbind(k, callback_example, *args)
-    # or:
-    #   i.unbind(k, "callback_example")
-    #   i.unbind(k, callback_example.__name__)
+def callback_example(i, k, infos, n, extra):
+    i.unbind(k, callback_example, extra)
     i.save("foo")
     i.send("msg", "haha")
 
 @register_log_callback
-def callback_example2(i, k, infos, n, *args):
+def callback_example2(i, k, infos, n, extra):
     i.save("foobar")
     i.save("a")
-
-
-def check(i, key, evaluation, env, callback, k):
-    daemon = "%s_daemon" % key
-    if eval(evaluation, None, env):
-        callback(key)
-        i.unbind(k, daemon, evaluation, callback)
-    else:
-        i.bind(k, daemon, evaluation, callback)
-
-
-def f(func_name):
-    print(func_name, "--> ok", file=sys.stderr)
-
-@register_log_callback
-def tower_daemon(i, k, infos, n, evaluation, callback):
-    #print(evaluation, callback, file=sys.stderr)
-    check(i, "tower", evaluation, None, callback, k)
 
 
 if __name__ == "__main__":
