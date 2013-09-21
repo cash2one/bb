f37e642e1adb9c44b6bea57bc5f3956dec9729dc
felloak <felloak@gmail.com>
2013-09-21 21:06:50 +0800

    util: tune build_dict and build_list

--- bb/util.py
+++ bb/util.py
@@ -11,7 +11,11 @@ def build_dict(title, key, values, value_wraps={}):
     {1: [1, 1], 2: [4, 8]}
     >>> build_dict(title, "x", ("y", "z"))
     {1: (1, 1), 2: (4, 8)}
-    >>> build_dict(title, "x", {"y", "z"}, wraps2) == \
+    >>> build_dict(title, "x", {"y", "z"}) == \
+    ... build_dict(title, "x", frozenset({"y", "z"})) == \
+    ... {1: {1}, 2: {4, 8}}
+    True
+    >>> build_dict(title, "x", dict.fromkeys(["y", "z"]), wraps2) == \
     ... {1: {"y": 1, "z": "1"}, 2: {"y": 4, "z": "8"}}
     True
     >>> build_dict(title, "x", ["y", "not_exist"])
@@ -27,17 +31,15 @@ def build_dict(title, key, values, value_wraps={}):
     if isinstance(values, str):
         for x in title:
             dct[x[key]] = wrap(values)
-    elif isinstance(values, list):
-        for x in title:
-            dct[x[key]] = list(wrap(k) for k in values)
-    elif isinstance(values, tuple):
-        for x in title:
-            dct[x[key]] = tuple(wrap(k) for k in values)
-    elif isinstance(values, set):
+    elif isinstance(values, dict):
         for x in title:
             dct[x[key]] = dict((k, wrap(k)) for k in values)
+    elif isinstance(values, (list, tuple, set, frozenset)):
+        _type = type(values)
+        for x in title:
+            dct[x[key]] = _type(wrap(k) for k in values)
     else:
-        raise ValueError(values)
+        raise TypeError(values)
     return dct
 
 def build_list(title, values, value_wraps={}):
@@ -50,6 +52,10 @@ def build_list(title, values, value_wraps={}):
     >>> build_list(title, ("y", "z"))
     [(1, 1), (4, 8)]
     >>> build_list(title, {"y", "z"}) == \
+    ... build_list(title, frozenset({"y", "z"})) == \
+    ... [{1}, {4, 8}]
+    True
+    >>> build_list(title, dict.fromkeys(["y", "z"])) == \
     ... [{'y': 1, 'z': 1}, {'y': 4, 'z': 8}]
     True
     >>> build_list(title, "null")
@@ -65,17 +71,15 @@ def build_list(title, values, value_wraps={}):
     if isinstance(values, str):
         for x in title:
             lst.append(wrap(values))
-    elif isinstance(values, list):
-        for x in title:
-            lst.append(list(wrap(k) for k in values))
-    elif isinstance(values, tuple):
-        for x in title:
-            lst.append(tuple(wrap(k) for k in values))
-    elif isinstance(values, set):
+    elif isinstance(values, dict):
         for x in title:
             lst.append(dict((k, wrap(k)) for k in values))
+    elif isinstance(values, (list, tuple, set, frozenset)):
+        _type = type(values)
+        for x in title:
+            lst.append(_type(wrap(k) for k in values))
     else:
-        raise ValueError(values)
+        raise TypeError(values)
     return lst
 
 
