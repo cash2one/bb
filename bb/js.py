286c134be47a09137808f7e9c8783b2457b1730e
felloak <felloak@gmail.com>
2013-09-23 16:40:02 +0800

    add bb.js
    my custom json dumps

---
+++ bb/js.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+r"""
+>>> dump1("中")
+'"中"'
+>>> dump1({1, 2})
+'[1,2]'
+>>> dump2({"a": 1, "b": 2})
+'{\n    "a": 1,\n    "b": 2\n}'
+"""
+
+import functools
+import json
+
+dump1 = functools.partial(json.dumps, ensure_ascii=False, default=list,
+                          separators = (",", ":"))
+
+dump2 = functools.partial(json.dumps, ensure_ascii=False, default=list,
+                          separators = (",", ": "), sort_keys=True, indent=4)
+
+
+if __name__ == "__main__":
+    print("doctest:")
+    import doctest
+    doctest.testmod()
