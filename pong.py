76d6a0158af401863dc5f5745ab23cd1ae2ee5f9
felloak <felloak@gmail.com>
2013-09-23 13:24:32 +0800

    rewrite handler "online"

--- pong.py
+++ pong.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 from bb.inst import instructions, processes, handle, run
 from bb.i import I, P
 
@@ -20,9 +22,20 @@ def ping(i, n):
     i.save("foo")
     return i.flush()
 
+@I.register_log_callback
+def _test(_, i, log, infos, n):
+    print(_, i, log, infos, n, i.listeners, i.logs, sep="\n")
+
 @handle
-def online(i, n):
-    i.online = n
+def online(i, b):
+    i.online = b
+    if b:
+        i.bind("online", "_test", "on")
+        i.bind("offline", "_test", "off")
+        i.log("online", {"time": time.time()})
+        # blabla...
+    else:
+        i.log("offline", {"time": time.time()})
 
 @run
 def plus():
