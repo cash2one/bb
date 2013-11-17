30c5a67374789478800c696e0f367042e36dcd1f
felloak <felloak@gmail.com>
2013-11-17 22:35:39 +0800

    improve tencent_demo

--- leader/tencent_demo.py
+++ leader/tencent_demo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import re
 
 import tornado.httpclient
 import tornado.ioloop
@@ -9,21 +10,20 @@ import tornado.options
 
 from tencent import mk_url
 
-args = [
-    "openid",
-    "openkey",
-    "pf",
-]
+openid_pattern = re.compile(r"^[0-9A-F]{32}$")
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         http = tornado.httpclient.AsyncHTTPClient()
-        url = mk_url(
-            {k: self.get_argument(k) for k in args},
-            "v3/user/get_info"
-            )
-        http.fetch(url, self.on_response)
+        args = {k: v[0].decode() for k, v in self.request.arguments.items()}
+        if args.get("pf") and \
+           args.get("openkey") and \
+           openid_pattern.match(args.get("openid", "")):
+            url = mk_url(args, "v3/user/get_info")
+            http.fetch(url, self.on_response)
+        else:
+            self.finish()
 
     def on_response(self, response):
         if response.error:
