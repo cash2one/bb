6e656e79c5444af0fbf1e9fc7a23c0e675829fe4
felloak <felloak@gmail.com>
2014-04-01 12:35:35 +0800

    test_module_t...

--- bb/test_module_t.c
+++ bb/test_module_t.c
@@ -1,12 +1,28 @@
 #include <Python.h>
 
-static PyObject* test(PyObject* self, PyObject* args) {
-	printf("%x, %x", self, args);
-	return PyLong_FromLong(123);
+#define MAP_MAX 10
+
+static short *m[MAP_MAX];
+
+static PyObject* t1(PyObject* self, PyObject* args) {
+	//printf("%lx, %lx", self, args);
+	printf("%lu, %lu\n", sizeof(m), sizeof(m[0][0]));
+	int i = m[0][0];
+	for (i = 0; i < MAP_MAX; i++) {
+		m[i] = calloc(sizeof(short), 8196);
+		//printf("%ld\n", m[i]);
+	}
+	return PyLong_FromLong(1);
+}
+
+PyObject* t2(PyObject* self, PyObject* args, PyObject* kwargs) {
+	//printf("%lx, %lx", self, args);
+	return PyLong_FromLong(2);
 }
 
 static PyMethodDef methods[] = {
-	{"f",  test, METH_VARARGS, ""},
+	{"f1",  t1, METH_VARARGS, "doc1, 一"},
+	//{"f2",  t2, METH_VARARGS | METH_KEYWORDS, "doc2"},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -19,6 +35,7 @@ static struct PyModuleDef speedupsmodule = {
 };
 
 PyMODINIT_FUNC
-PyInit_t() {
+PyInit_t(void) {
+	printf("init!\n");
 	return PyModule_Create(&speedupsmodule);
 }
