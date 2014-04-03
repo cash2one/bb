e09d963be7e4be2b1e4606c14ca1c93d4779128a
felloak <felloak@gmail.com>
2014-04-03 18:27:28 +0800

    improve cli

--- cli.py
+++ cli.py
@@ -5,13 +5,32 @@
 # (cat in; sleep 10) | nc localhost 2000 >out2
 # (cat in; sleep 10) | nc localhost 2000 >out3
 
+import json
+import struct
+import sys
+
+from bb.msg import dump1
+from bb.const import INSTRUCTIONS, STRUCT
+
+HEAD = """tgw_l7_forward\r
+Host:z.app.twsapp.com\r
+1 token\r
+\r
+"""
+
 if __name__ == "__main__":
-    import sys
-    i = True
-    while i:
+    sys.stdout.write(HEAD)
+    while True:
         try:
-            i = input()
+            s = input()
         except (KeyboardInterrupt, EOFError):
             break
-        l = len(i)
-        sys.stdout.write(chr(l//256) + chr(l%256) + i)
+        if not s:
+            break
+        inst, data = (s.split(None, 1) + [None])[:2]
+        inst = INSTRUCTIONS[inst]
+        data = dump1(eval(data)).encode() if data else b''
+        #print(inst, data)
+        sys.stdout.buffer.write(
+            struct.pack(STRUCT, len(data) + 2, inst) + data)
+
