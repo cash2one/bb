9d3420fba7bb9945d015ab87d5db1217dd808155
Weizhao Li <lwzm@qq.com>
2014-05-09 14:06:15 +0800

    send ajax request thriftily
    remove mouse event in time

--- static/show.js
+++ static/show.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-        $("ul").children().children().mouseover(function() {
-            var a = $(this);
+    $("ul").children().children().on("mouseover", function() {
+        var a = $(this);
+        if (!a.attr("title")) {
+            a.off("mouseover");
             $.get("/hub_eval" + a.attr("href"), function(data) {
                 a.attr("title", data);
-                }, "text");
-            });
-        });
+            }, "text");
+        }
+    });
+});
