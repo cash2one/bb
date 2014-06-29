3f1392670740f16d726cd5156c2d33a5eeb3e412
felloak <felloak@gmail.com>
2014-06-29 09:52:15 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    from setuptools import setup
-    from distutils.core import Extension
-    name = "bb"
-    version = "1"
-    setup(name=name, version=version, packages=[name],
-          ext_modules=[Extension("bb.t", sources=["bb/test_module_t.c"])],
-         )
+from distutils.core import setup, Extension
+name = "bb"
+version = "1"
+setup(
+    name=name,
+    version=version,
+    packages=[name],
+    scripts=[name[0]],
+    )
